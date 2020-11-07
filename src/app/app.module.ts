fb69b8854e4f1b6d2a1fa2f19aa56eee8b8986e0
ieneka <60981008+ieneka@users.noreply.github.com>
2020-11-07 21:42:00 +0100

    Complete intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { WellcomeComponent } from './components/home/wellcome/wellcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    WellcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { trigger, state, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('buttonState', [
+      state('inactive', style({
+        opacity: 0,
+        transform: 'scale(0.1)'
+      })),
+      state('active',   style({
+        opacity: 1,
+        transform: 'scale(1)'
+      })),
+      transition('inactive => active', animate('6s 13000ms')),
+      transition('active => inactive', animate('6s 13000ms'))
+    ]),
+  ]
 })
-export class HomeComponent implements OnInit {
 
+export class HomeComponent implements OnInit {
+  state = 'inactive';
+  display = true;
+  @ViewChild('videoRef') private videoRef: ElementRef<HTMLVideoElement>;
+  @ViewChild('musicRef') private musicRef: ElementRef<HTMLAudioElement>;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  togleButton(): void{
+    this.state = this.state === 'inactive' ? 'active' : 'inactive';
+  }
+  noneDisplay(): void{
+    this.videoRef.nativeElement.play();
+    this.musicRef.nativeElement.play();
+
+    this.display = false;
   }
 
 }
