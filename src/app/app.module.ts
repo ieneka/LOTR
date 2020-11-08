f0582a3808d60414cf795f3d582f8e476c3567f7
ieneka <60981008+ieneka@users.noreply.github.com>
2020-11-08 13:58:00 +0100

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { WellcomeComponent } from './components/home/wellcome/wellcome.component';
+import { NavbarComponent } from './components/home/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    WellcomeComponent
+    WellcomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
