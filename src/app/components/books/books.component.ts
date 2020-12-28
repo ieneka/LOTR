9b6622e6027417fd8974b42967aebf944be5309c
ieneka <ienekamartinezg@gmail.com>
2020-12-28 19:14:07 +0100

    complete books

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -10,43 +10,120 @@ import { ApiService } from '../../services/api.service';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
- books: BooksI;
- bookName: string[];
- bookIds: string[];
+ books: [];
  name;
  book1 = false;
  book2 = false;
  book3 = false;
- selectedValue = "";
+ book4 = false;
+ book5 = false;
+ book6 = false;
+ book7 = false;
+ book8 = false;
+ selectedValue : BooksI;
+ booksApi = [];
+ click = false;
+ hobit1 = {_id:"5cf58080b53e011a64671585", name:"The Hobbit, Part One"}
+ hobit2 = {_id:"5cf58080b53e011a64671586", name:"The Hobbit, Part Two"}
+ silmarilion = {_id:"5cf58080b53e011a64671587", name:"The Silmarillion"};
+ children = {_id:"5cf58080b53e011a64671588", name:"The Children of Húrin"};
+ beren = {_id:"5cf58080b53e011a64671589", name:"Beren and Lúthien "};
 
   constructor(private api: ApiService) {
-    this.name = new FormControl('');
   }
 
-  // tslint:disable-next-line: typedef
   ngOnInit() {
     this.api.getBooks().subscribe( data => {
-      // tslint:disable-next-line: no-string-literal
       this.books = data['docs'];
-      this.bookName = [this.books[0].name, this.books[1].name, this.books[2].name];
-      this.bookIds = [this.books[0]._id, this.books[1]._id, this.books[2]._id];
+      this.booksApi.push(this.books);
+      this.booksApi[0].push(this.hobit1,this.hobit2, this.silmarilion, this.children, this.beren);
+      
     });
+    console.log(this.booksApi)
   }
   selectValue() {
-    if (this.name.value === "The Fellowship Of The Ring"){
+    this.click = true;
+    this.selectedValue = this.name;
+    console.log(this.selectedValue)
+    if (this.selectedValue.name === "The Fellowship Of The Ring"){
       this.book1 = true;
       this.book2 = false;
       this.book3 = false;
+      this.book4 = false;
+      this.book5 = false;
+      this.book6 = false;
+      this.book7 = false;
+      this.book8 = false;
     }
-    else if (this.name.value === "The Two Towers"){
+    else if (this.selectedValue.name === "The Two Towers"){
       this.book1 = false;
       this.book2 = true;
       this.book3 = false;
+      this.book4 = false;
+      this.book5 = false;
+      this.book6 = false;
+      this.book7 = false;
+      this.book8 = false;
     }
-    else {
+    else if (this.selectedValue.name === "The Return Of The King"){
       this.book1 = false;
       this.book2 = false;
       this.book3 = true;
+      this.book4 = false;
+      this.book5 = false;
+      this.book6 = false;
+      this.book7 = false;
+      this.book8 = false;
+    }
+    else if (this.selectedValue.name === "The Hobbit, Part One"){
+      this.book1 = false;
+      this.book2 = false;
+      this.book3 = false;
+      this.book4 = true;
+      this.book5 = false;
+      this.book6 = false;
+      this.book7 = false;
+      this.book8 = false;
+    }
+    else if (this.selectedValue.name === "The Hobbit, Part Two"){
+      this.book1 = false;
+      this.book2 = false;
+      this.book3 = false;
+      this.book4 = false;
+      this.book5 = true;
+      this.book6 = false;
+      this.book7 = false;
+      this.book8 = false;
+    }
+    else if (this.selectedValue.name === "The Silmarillion"){
+      this.book1 = false;
+      this.book2 = false;
+      this.book3 = false;
+      this.book4 = false;
+      this.book5 = false;
+      this.book6 = true;
+      this.book7 = false;
+      this.book8 = false;
+    }
+    else if (this.selectedValue.name === "The Children of Húrin"){
+      this.book1 = false;
+      this.book2 = false;
+      this.book3 = false;
+      this.book4 = false;
+      this.book5 = false;
+      this.book6 = false;
+      this.book7 = true;
+      this.book8 = false;
+    }
+    else{
+      this.book1 = false;
+      this.book2 = false;
+      this.book3 = false;
+      this.book4 = false;
+      this.book5 = false;
+      this.book6 = false;
+      this.book7 = false;
+      this.book8 = true;
     }
   }
  
